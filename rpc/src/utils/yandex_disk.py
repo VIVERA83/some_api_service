ec0651e92f8c0225ce334cda5e4465f78b6f989f
Vivera83 <vivera83@yandex.ru>
2022-08-04 10:59:55 +0300

    Readme.md - описана суть проекта, дополнины описание YandexDisk

--- rpc/src/utils/yandex_disk.py
+++ rpc/src/utils/yandex_disk.py
@@ -11,16 +11,25 @@ class YandexDisk:
         self.disk = YaDisk(secret=secret, token=token)
 
     async def upload(self, fd: io.BytesIO, file_name: str):
-        """Принимает изображение переданное в bytes, наносит текст (text) на картинку, далее изображение сохраняется на
-        YandexDisk"""
+        """
+        Принимает изображение переданное в bytes, наносит текст (text) на картинку, далее изображение сохраняется на
+        YandexDisk.
+        :param fd: Файл, в виде байтового объекта.
+        :param file_name: Имя файла, под которым будет хранится файл в Yandex Disk.
+        :return:
+        """
         await self.disk.upload(fd, "dating_images/" + file_name)
 
     async def download(self, path: str) -> bytes:
-        """Загрузить изображение с диска"""
+        """
+        Загрузить изображение с диска
+        :param path: Путь к файлу в Yandex Disk
+        :return: файл в виде bytes
+        """
         file = io.BytesIO()
         await self.disk.download(path, file)
         return file.getvalue()
 
     async def close(self):
-        """Корректное закрытие диска"""
+        """Корректное закрытие соединений с диском"""
         await self.disk.close()
