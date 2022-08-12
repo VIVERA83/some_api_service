e69ac33d61f43d57e18053a85fbeb2158e67d4bd
Vivera83 <vivera83@yandex.ru>
2022-08-12 20:42:31 +0300

    добавлена моделька GeoData

--- dating_site/src/models/user_model.py
+++ dating_site/src/models/user_model.py
@@ -19,6 +19,7 @@ class User(Base):
     password = Column(String(), nullable=False)
 
     avatars = relationship("Avatar", lazy="joined", cascade="all,delete", uselist=True)
+    geodata = relationship("Geodata", lazy="joined", cascade="all,delete", uselist=True)
 
 
 class Avatar(Base):
@@ -29,4 +30,16 @@ class Avatar(Base):
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
 
 
-MODELS = Union[User]
+class GeoData(Base):
+    __tablename__ = "geodata"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, unique=True, nullable=False)
+    geodata = Column(String(), nullable=False)
+    city = Column(String(), nullable=False)
+    latitude = Column(String(), nullable=False)
+    longitude = Column(String(), nullable=False)
+
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+
+
+MODELS = Union[User, Avatar]
